004b987eab8b711dfe5628fb0f63b7d06bff8e37
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-07-29 17:15:43 +0700

    init file

---
+++ src/main/java/com/Springbootstarter1Application.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootstarter1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootstarter1Application.class, args);
+	}
+
+}
